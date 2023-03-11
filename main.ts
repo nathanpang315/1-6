7aa2133f22f8e5847eb587dcc857f141b87653f5
nathanpang315 <125708023+nathanpang315@users.noreply.github.com>
2023-03-11 11:41:27 +0800

    MakeCode 專案的初始檔案

---
+++ main.ts
@@ -0,0 +1,60 @@
+input.onButtonPressed(Button.A, function () {
+    Fit += randint(1, 6)
+    basic.showNumber(Fit)
+    if (Fit == 1) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . # . .
+            . . . . .
+            . . . . .
+            `)
+    }
+    if (Fit == 2) {
+        basic.showLeds(`
+            . . . . .
+            . . # . .
+            . . . . .
+            . . # . .
+            . . . . .
+            `)
+    }
+    if (Fit == 3) {
+        basic.showLeds(`
+            . . . . .
+            . . . # .
+            . . # . .
+            . # . . .
+            . . . . .
+            `)
+    }
+    if (Fit == 4) {
+        basic.showLeds(`
+            . . . . .
+            . # . # .
+            . . . . .
+            . # . # .
+            . . . . .
+            `)
+    }
+    if (Fit == 5) {
+        basic.showLeds(`
+            . . . . .
+            . # . # .
+            . . # . .
+            . # . # .
+            . . . . .
+            `)
+    }
+    if (Fit == 6) {
+        basic.showLeds(`
+            . . . . .
+            . # . # .
+            . # . # .
+            . # . # .
+            . . . . .
+            `)
+    }
+})
+let Fit = 0
+Fit = 0
